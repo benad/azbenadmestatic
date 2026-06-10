5b8e8df87d1d5cdcadcf6468f112dcc57a95cfcd
Benoit Nadeau <benad@me.com>
2026-06-10 16:52:24 -0400

    Replace JSDOM with xmldom and a custom HTML serializer
    Avoid the heavy JSDOM dependency tree by replacing it with @xmldom/xmldom, which has zero dependencies. To bypass the XML serializer outputting self-closing tags (which breaks browser parsing), a custom HTML serializer is used to walk the DOM tree and serialize it with correct HTML formatting rules.
    
    Co-authored-by: Antigravity CLI <antigravity-support@google.com>

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,7 @@
 const { default: pluginRss } = require("@11ty/eleventy-plugin-rss");
 const { DateTime } = require("luxon");
 const { XSLTProcessor } = require("@tradik/xslt-processor");
-const { JSDOM } = require("jsdom");
+const { DOMImplementation, DOMParser } = require("@xmldom/xmldom");
 const toml = require("@iarna/toml");
 const AdmZip = require('adm-zip');
 const fs = require('node:fs');
@@ -11,6 +11,80 @@ const twitterZip = 'src/twitter.zip'
 var parseDate = function (dateObj, zone) {
 	return DateTime.fromJSDate(dateObj, { zone: zone || "America/New_York" });
 };
+
+// Custom HTML Serializer to serialize W3C XML DOM into browser-compatible HTML
+function serializeToHTML(node, isRoot = false) {
+	if (!node) return "";
+
+	switch (node.nodeType) {
+		case 1: { // Element
+			const tagName = node.tagName.toLowerCase();
+			let attrs = "";
+			if (node.attributes) {
+				for (let i = 0; i < node.attributes.length; i++) {
+					const attr = node.attributes[i];
+					attrs += ` ${attr.name}="${escapeAttr(attr.value)}"`;
+				}
+			}
+
+			const voidElements = new Set([
+				"area", "base", "br", "col", "embed", "hr", "img", "input",
+				"link", "meta", "param", "source", "track", "wbr"
+			]);
+
+			if (voidElements.has(tagName)) {
+				return `<${tagName}${attrs}>`;
+			}
+
+			let children = "";
+			if (node.childNodes) {
+				for (let i = 0; i < node.childNodes.length; i++) {
+					children += serializeToHTML(node.childNodes[i], false);
+				}
+			}
+
+			return `<${tagName}${attrs}>${children}</${tagName}>`;
+		}
+
+		case 3: // Text
+			if (isRoot && /^\s*$/.test(node.nodeValue)) {
+				return "";
+			}
+			return escapeText(node.nodeValue);
+
+		case 8: // Comment
+			return `<!--${node.nodeValue}-->`;
+
+		case 9: // Document
+		case 11: { // DocumentFragment
+			let children = "";
+			if (node.childNodes) {
+				for (let i = 0; i < node.childNodes.length; i++) {
+					children += serializeToHTML(node.childNodes[i], true);
+				}
+			}
+			return children;
+		}
+
+		default:
+			return "";
+	}
+}
+
+function escapeText(str) {
+	if (typeof str !== "string") return "";
+	return str
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/>/g, "&gt;");
+}
+
+function escapeAttr(str) {
+	if (typeof str !== "string") return "";
+	return str
+		.replace(/&/g, "&amp;")
+		.replace(/"/g, "&quot;");
+}
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = function (eleventyConfig) {
@@ -74,31 +148,27 @@ module.exports = function (eleventyConfig) {
 
 			const xslText = fs.readFileSync(xslpath, "utf-8");
 
-			const dom = new JSDOM("<!DOCTYPE html><html><body></body></html>", { contentType: "text/html" });
+			const impl = new DOMImplementation();
+			const doc = impl.createDocument(null, null, null);
 
 			// Expose DOM globals for xslt-processor
 			const originalDocument = global.document;
 			const originalDOMParser = global.DOMParser;
-			const originalXMLSerializer = global.XMLSerializer;
 
-			global.document = dom.window.document;
-			global.DOMParser = dom.window.DOMParser;
-			global.XMLSerializer = dom.window.XMLSerializer;
+			global.document = doc;
+			global.DOMParser = DOMParser;
 
 			let html;
 			try {
-				const parser = new dom.window.DOMParser();
+				const parser = new DOMParser();
 				const xslDoc = parser.parseFromString(xslText, "application/xml");
 				const xmlDoc = parser.parseFromString(inputContent, "application/xml");
 
 				const processor = new XSLTProcessor();
 				processor.importStylesheet(xslDoc);
 
-				const fragment = processor.transformToFragment(xmlDoc, dom.window.document);
-				const tempDiv = dom.window.document.createElement("div");
-				tempDiv.appendChild(fragment);
-
-				html = tempDiv.innerHTML;
+				const fragment = processor.transformToFragment(xmlDoc, doc);
+				html = serializeToHTML(fragment);
 
 				if (!html.startsWith("<!DOCTYPE")) {
 					html = "<!DOCTYPE html>\n" + html;
@@ -106,7 +176,6 @@ module.exports = function (eleventyConfig) {
 			} finally {
 				global.document = originalDocument;
 				global.DOMParser = originalDOMParser;
-				global.XMLSerializer = originalXMLSerializer;
 			}
 
 			return (data) => {
